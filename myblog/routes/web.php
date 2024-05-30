582cee8896896545da6595b98cb3fbe46257762c
Alter1412 <florianalaforgia@hotmail.com.ar>
2024-05-29 21:52:44 -0300

    Rutas y controladores de rutas agregados

--- myblog/routes/web.php
+++ myblog/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'getHome']);
+
+/*
+mas adelante
+Route::get('login', function () {
+    return view('login');
 });
+Route::get('logout', function () {
+    return view('logout');
+}); */
+
+Route::get('category', [CategoryController::class,'getIndex']);
+
+Route::get('category/show/{id}', [CategoryController::class,'getShow']);
+
+Route::get('category/create', [CategoryController::class, 'getCreate']);
+
+Route::get('category/edit/{id}', [CategoryController::class,'getEdit']);
+
+
